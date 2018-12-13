a1a4c18cb039b13095279cd81fe62daef686d221
georgechan74 <georgechan74@gmail.com>
2018-12-13 10:35:00 -0800

    added add/remove card function

--- app/src/main/java/com/example/androidprojtest2/MainActivity.java
+++ app/src/main/java/com/example/androidprojtest2/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.androidprojtest2;
 
-import android.app.Activity;
 import android.app.PendingIntent;
-import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.media.Image;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
@@ -21,8 +18,6 @@ import android.nfc.tech.NfcF;
 import android.nfc.tech.NfcV;
 import android.os.AsyncTask;
 import android.os.Parcelable;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -35,18 +30,15 @@ import android.widget.Toast;
 
 import com.example.androidprojtest2.data.ScanLog;
 import com.example.androidprojtest2.data.ScanLogViewModel;
-import com.example.androidprojtest2.database.Identification;
 import com.example.androidprojtest2.database.IdentificationViewModel;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -55,13 +47,12 @@ public class MainActivity extends AppCompatActivity {
     private ImageView mFail;
     private ImageView mSuccess;
     private IdentificationViewModel mViewModel;
-    private List<Identification> mAllIDs = new ArrayList<>();
-    private Identification mIdentification = null;
     private ScanLogViewModel mScanLogViewModel;
     private FirebaseDatabase mDatabase;
     private DatabaseReference mMyRef;
-    private boolean mDoesExist = false;
-    private ArrayList<String> mValues;
+    private boolean mAddItem = false;
+    private boolean mRemoveItem = false;
+    private HashMap<String, String> mValues;
 
     private final String[][] techList = new String[][] {
             new String[] {
@@ -83,10 +74,6 @@ public class MainActivity extends AppCompatActivity {
         mDatabase = FirebaseDatabase.getInstance();
         mMyRef = mDatabase.getReference();
 
-//        Log.d(TAG, "FirebaseDatabase: " + database.toString() + ", DatabaseReference: " + myRef.toString());
-
-//        myRef.setValue("Hello, World!");
-
         mStatus = (TextView) findViewById(R.id.status);
         mFail = (ImageView) findViewById(R.id.fail);
         mSuccess = (ImageView) findViewById(R.id.success);
@@ -96,9 +83,6 @@ public class MainActivity extends AppCompatActivity {
 
         mViewModel.insert("Hello!");
 
-        //call addvalueeventlistenet to store all values into a list
-        //inside the asynctask to check the database, check the list instead; it has all the values
-
         mMyRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -107,15 +91,15 @@ public class MainActivity extends AppCompatActivity {
 //                String value = dataSnapshot.getValue(String.class);
 //                Log.d(TAG, "Value is: " + value);
 
-                ArrayList<String> values = new ArrayList<>();
+                HashMap<String,String> values = new HashMap<>();
 
                 for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                    String description = ds.getValue(String.class);
-                    values.add(description);
-                    Log.d("descriptionvalue", description);
+//                    String description = ds.getValue(String.class);
+                    values.put(ds.getKey(),ds.getValue(String.class));
+                    Log.d("descriptionvalue", ds.getValue(String.class));
                 }
 
-                mValues = new ArrayList<>(values);
+                mValues = values;
             }
 
             @Override
@@ -124,26 +108,6 @@ public class MainActivity extends AppCompatActivity {
                 Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
-
-//        Log.d("sizeofvalues", mValues.size()+"");
-
-//        mMyRef.addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                for (DataSnapshot ds : dataSnapshot.getChildren()) {
-//                    String description = ds.getValue(String.class);
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//                Log.d("databaseerror", databaseError.getMessage());
-//            }
-//        });
-    }
-
-    private void updateList(List<Identification> identifications) {
-        mAllIDs.addAll(identifications);
     }
 
     @Override
@@ -193,7 +157,7 @@ public class MainActivity extends AppCompatActivity {
                 // Process the messages array.
             }
 
-            outputResult(finalMessage);
+            decideAction(finalMessage);
         }
         Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
     }
@@ -260,7 +224,77 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         }
+        else if (itemThatWasClickedId == R.id.addButton){
+            mAddItem = !mAddItem;
+            mRemoveItem = false;
+            Toast.makeText(MainActivity.this, "The next card scanned will be added to the database.", Toast.LENGTH_LONG).show();
+        }
+        else if (itemThatWasClickedId == R.id.removeButton){
+            mAddItem = false;
+            mRemoveItem = !mRemoveItem;
+            Toast.makeText(MainActivity.this, "The next card scanned will be removed from the database.", Toast.LENGTH_LONG).show();
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void decideAction(String output){
+        if (mAddItem)
+            add(output);
+        else if(mRemoveItem)
+            remove(output);
+        else
+            outputResult(output);
+
+        mAddItem = false;  // Only add or remove one (the next) item
+        mRemoveItem = false;
+    }
+
+    protected void add(String output){
+        if (mValues.containsValue(output))
+        {
+            Toast.makeText(MainActivity.this, "Card is already in database, cancelling add.", Toast.LENGTH_LONG).show();
+            mFail.setVisibility(View.VISIBLE);
+            mStatus.setText("Duplicate card found");
+        }
+
+        else {
+//            Toast.makeText(MainActivity.this, "Card is not in database, " +output+ " will be added." , Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this, "Card is not in database and will be added." , Toast.LENGTH_LONG).show();
+            mSuccess.setVisibility(View.VISIBLE);
+            mStatus.setText("Added!");
+            DatabaseReference tempRef = mMyRef.child("Tag"+(mValues.size()+1));
+            tempRef.setValue(output);
+        }
+        //makes message linger for 3 seconds
+        Sleeper sleeper = new Sleeper();
+        sleeper.execute();
+    }
+
+    protected void remove(String output){
+        if (!mValues.containsValue(output))
+        {
+            Toast.makeText(MainActivity.this, "Card is not in database, cancelling remove.", Toast.LENGTH_LONG).show();
+            mFail.setVisibility(View.VISIBLE);
+            mStatus.setText("Card not found");
+        }
+        else{
+//            Toast.makeText(MainActivity.this, "Card is in the database, " +output+ " will be removed.", Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this, "Card is in the database and will be removed.", Toast.LENGTH_LONG).show();
+            mSuccess.setVisibility(View.VISIBLE);
+            mStatus.setText("Removed!");
+            String tagName = null;
+            for(String s : mValues.keySet()){
+                if(mValues.get(s).equals(output)){
+                    tagName = s;
+                    break;
+                }
+            }
+            mMyRef.child(tagName).removeValue();
+        }
+        //makes message linger for 3 seconds
+        Sleeper sleeper = new Sleeper();
+        sleeper.execute();
+
     }
 
     public void outputResult(String output) {
@@ -268,7 +302,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d("outoutresult", output);
         GetIdentification getIdentification = new GetIdentification();
         getIdentification.execute(output);
-        if (mValues.contains(output)) {
+        if (mValues.containsValue(output)) {
             Toast.makeText(MainActivity.this, "Card in database. Scan activity logged to history.", Toast.LENGTH_LONG).show();
             mSuccess.setVisibility(View.VISIBLE);
             mStatus.setText("Success!");
@@ -286,39 +320,11 @@ public class MainActivity extends AppCompatActivity {
     class GetIdentification extends AsyncTask<String, Void, Void> {
         @Override
         protected Void doInBackground(final String... strings) {
-//            mIdentification = mViewModel.checkDatabase(strings[0]);
-//            checkFirebase(strings[0]);
             Log.d("isitchecking", "yes");
             ScanLog scanLog = new ScanLog(strings[0], new Date());
             mScanLogViewModel.insert(scanLog);
             return null;
         }
-
-//        @Override
-//        protected void onPostExecute(Void aVoid) {
-//            super.onPostExecute(aVoid);
-////            if (mIdentification == null){
-////
-////                Toast.makeText(MainActivity.this, "Card not in database. Scan activity logged to history.", Toast.LENGTH_LONG).show();
-////                mFail.setVisibility(View.VISIBLE);
-////                mStatus.setText("Failed...");
-////            }
-////            else
-////            {
-////                Toast.makeText(MainActivity.this, "Card in database. Scan activity logged to history.", Toast.LENGTH_LONG).show();
-////                mSuccess.setVisibility(View.VISIBLE);
-////                mStatus.setText("Success!");
-////                mIdentification = null;
-////            }
-//            if (!mDoesExist) {
-//                Toast.makeText(MainActivity.this, "Card not in database. Scan activity logged to history.", Toast.LENGTH_LONG).show();
-//                mFail.setVisibility(View.VISIBLE);
-//                mStatus.setText("Failed...");
-//            }
-//            else {
-//                mDoesExist = false;
-//            }
-//        }
     }
 
     class Sleeper extends AsyncTask<Void, Void, Void> {
@@ -336,52 +342,9 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Void aVoid) {
             super.onPostExecute(aVoid);
-                mFail.setVisibility(View.GONE);
-                mSuccess.setVisibility(View.GONE);
-                mStatus.setText("Awaiting Scan...");
+            mFail.setVisibility(View.GONE);
+            mSuccess.setVisibility(View.GONE);
+            mStatus.setText("Awaiting Scan...");
         }
-    }
-
-//    private void checkFirebase(final String input){
-//        mMyRef.addListenerForSingleValueEvent(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                for (DataSnapshot ds : dataSnapshot.getChildren()) {
-//                    String description = ds.getValue(String.class);
-//
-//                    if (description.equals(input)) {
-//                        Log.d("doesexist", "yes");
-//                        mDoesExist = true;
-//                    }
-//                    Log.d("isiterating", "yes");
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//                Log.d("databaseerror", databaseError.getMessage());
-//            }
-//        });
-//    }
-
-    public void checkFirebase(String input) {
-        Log.d("enteredcriticalarea", "true");
-        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference();
-        Query query = myRef.child("securitybadge").equalTo(input);
-        query.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    Log.d("foundamatch", "true");
-                    mDoesExist = true;
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.d("databaseerror", databaseError.getMessage());
-            }
-        });
-        Log.d("leavingcriticalarea", "true");
     }
 }
